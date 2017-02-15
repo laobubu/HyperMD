c79b3dd14e47288b313f2c7eaf93ec579a1d9eeb
laobubu <laobubu@gmail.com>
2017-02-15 14:33:24 +0800

    update test page

--- demo/index2.js
+++ demo/index2.js
@@ -35,20 +35,37 @@ click_bind("hypermd_mode", function () {
     editor.setOption('hmdHideToken', '(profile-1)')
 })
 
-var name_comment = {
-    clike: "c, cpp, c-like",
-    octave: "octave / MATLAB",
-    htmlembedded: "HTMLembedded (JSP, ASP.NET)",
-    gas: "Gas (AT&T-style assembly)"
-}
 var $load_mode = document.getElementById('load_mode')
-'apl,asciiarmor,asn.1,asterisk,brainfuck,clike,clojure,cmake,cobol,coffeescript,commonlisp,crystal,css,cypher,d,dart,diff,django,dockerfile,dtd,dylan,ebnf,ecl,eiffel,elm,erlang,factor,fcl,forth,fortran,gas,gherkin,go,groovy,haml,handlebars,haskell,haskell-literate,haxe,htmlembedded,htmlmixed,http,idl,javascript,jinja2,jsx,julia,livescript,lua,mathematica,mbox,mirc,mllike,modelica,mscgen,mumps,nginx,nsis,ntriples,octave,oz,pascal,pegjs,perl,php,pig,powershell,properties,protobuf,pug,puppet,python,q,r,rpm,rst,ruby,rust,sas,sass,scheme,shell,sieve,slim,smalltalk,smarty,solr,soy,sparql,spreadsheet,sql,stex,stylus,swift,tcl,textile,tiddlywiki,tiki,toml,tornado,troff,ttcn,ttcn-cfg,turtle,twig,vb,vbscript,velocity,verilog,vhdl,vue,webidl,xml,xquery,yacas,yaml,yaml-frontmatter,z80'.split(',')
-    .forEach(function (name) {
-        var opt = document.createElement('option')
-        opt.textContent = name_comment[name] || name
-        opt.value = name
-        $load_mode.appendChild(opt)
-    })
+
+!function LoadModeList() {
+    var name_alias = {
+        clike: "c,cpp,csharp,scala,java,kotlin,nesc,objectivec,squirrel,ceylon",
+        octave: "matlab",
+        htmlembedded: "jsp",
+        htmlembedded: "asp.net",
+        gas: "AT&T ASM"
+    }, name_alias_rev = {}, modes = ''
+
+    for (var name in name_alias) {
+        name_alias[name].split(/,\s*/g).forEach(function (alias) {
+            name_alias_rev[alias] = name
+            modes += alias + ' (from ' + name + '),'
+        })
+    }
+
+    modes +=
+        'apl,asciiarmor,asn.1,asterisk,brainfuck,clike,clojure,cmake,cobol,coffeescript,commonlisp,crystal,css,cypher,d,dart,diff,django,dockerfile,dtd,dylan,ebnf,ecl,eiffel,elm,erlang,factor,fcl,forth,fortran,gas,gherkin,go,groovy,haml,handlebars,haskell,haskell-literate,haxe,htmlembedded,htmlmixed,http,idl,javascript,jinja2,jsx,julia,livescript,lua,mathematica,mbox,mirc,mllike,modelica,mscgen,mumps,nginx,nsis,ntriples,octave,oz,pascal,pegjs,perl,php,pig,powershell,properties,protobuf,pug,puppet,python,q,r,rpm,rst,ruby,rust,sas,sass,scheme,shell,sieve,slim,smalltalk,smarty,solr,soy,sparql,spreadsheet,sql,stex,stylus,swift,tcl,textile,tiddlywiki,tiki,toml,tornado,troff,ttcn,ttcn-cfg,turtle,twig,vb,vbscript,velocity,verilog,vhdl,vue,webidl,xml,xquery,yacas,yaml,yaml-frontmatter,z80'
+
+    modes.split(',')
+        .sort()
+        .forEach(function (name) {
+            var opt = document.createElement('option')
+            opt.textContent = name
+            opt.value = name_alias_rev[name] || name
+            $load_mode.appendChild(opt)
+        })
+}()
+
 click_bind("load_mode", function () {
     var mode = $load_mode.value, fail
     $load_mode.setAttribute('disabled', 'true')
